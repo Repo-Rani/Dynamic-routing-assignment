d3d8d217d1d042cb63fd2344a25e5a093b7331e5
Repo-Rani <ranimiss968@gmail.com>
2024-10-04 12:13:11 -0700

    some changes

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import React from 'react'
 
-const Button = () => {600
+const Button = () => {
   return (
     <div className=' bg-purple-900 border-2 border-purple-950 text-lg mt-4 py-2 px-2 rounded-xl hover:bg-purple-600 hover:p3'>
 <button>
